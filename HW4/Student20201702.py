db7ba9579cbd17dc9ac7fb5299c7e4064c47051d
dohee <dohee930@gmail.com>
2022-12-24 14:47:35 +0900

    hw4

--- HW4/Student20201702.py
+++ HW4/Student20201702.py
@@ -1,4 +1,30 @@
 import sys
-train = sys.argv[1]
-test = sys.argv[2]
+import kNN
+import numpy as np
+from os import listdir
+import sys
+
+    
+trainfile = sys.argv[1]
+testfile = sys.argv[2]
+
+test = listdir(testfile)
+m = len(test)
+reList = []
+error = 0
+errorList = []
+hwLabels, trainingMat = kNN.trainMat(trainfile)
+for k in range(1,21):
+	for testtxt in test:
+		result = (kNN.hwClassifier(trainingMat, hwLabels, testfile, testtxt, k))
+		testtxt_name = testtxt.split('.')[0]
+		testtxt_num = int(testtxt_name.split('_')[0])
+		if result != testtxt_num:
+			error += 1
+	errorList.append(int(error/m*100))
+	
+
+for i in errorList:
+	print(i)
+
 
